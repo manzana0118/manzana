90981de1041a09c29bf1cac6f86757dcff3d3869
manzana00 <dev.manzana00@gmail.com>
2022-04-29 18:01:28 +0900

    위로가기 및 헤더 고정 기능 구현

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
 $(document).ready(function () {
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: ['책임감', '성실성', '도전정신', '전문성', '소통능력'],
-        datasets: [{
-            label: 'possibility',
-            data: [95, 90, 89, 78, 85],
-            backgroundColor: [
-                'rgba(252, 136, 123, 0.2)',
-            ],
-            borderColor: [
-                'rgba(252, 136, 123, 1)',
-            ],
-            borderWidth: 2
-        }]
-    },
-    options: {
+
+    // 위로가기
+    let gotop = $('.gotop');
+    gotop.click(function(){
+        $('html').animate({
+            scrollTop: 0
+        }, 200);
+    });
+
+
+    // 헤더
+    let header = $('.header');
+    let h_logo = $('.h-logo');
+    let gnb = $('.gnb')
+
+    // 헤더 고정
+    $(window).scroll(function () {
+        let temp = $(window).scrollTop();
+        if (temp > 195) {
+            header.addClass('header-fixed');
+            h_logo.addClass('h-logo-fixed');
+            gnb.addClass('gnb-fixed');
+        } else {
+            header.removeClass('header-fixed');
+            h_logo.removeClass('h-logo-fixed');
+            gnb.removeClass('gnb-fixed');
+        }
+    });
+
+
+    // 가능성
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ['책임감', '성실성', '도전정신', '전문성', '소통능력'],
+            datasets: [{
+                label: 'possibility',
+                data: [95, 90, 89, 78, 85],
+                backgroundColor: [
+                    'rgba(252, 136, 123, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(252, 136, 123, 1)',
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
             responsive: true,
             layout: {
                 padding: 50,
@@ -32,8 +63,7 @@ const myChart = new Chart(ctx, {
                         font: {
                             size: 16,
                             family: 'Noto Sans KR',
-                            color: 'black',
-                            weight: 'bold',
+                            color: 'black'
                         }
                     },
                     beginAtZero: true,
@@ -42,10 +72,25 @@ const myChart = new Chart(ctx, {
             ticks: {
                 suggestedMin: 0,
                 suggestedMax: 100,
-                stepSize: 5, 
-                maxTicksLimit: 10, 
+                stepSize: 5,
+                maxTicksLimit: 10,
                 display: false
             }
         }
+    });
+
+    // Portfolio 탭 메뉴 구현
+    let p_button = $('.nav-pills .nav-link');
+    let p_contents = $('.tab-pane');
+    p_contents.eq(0).show();
+
+    console.log(p_button);
+    $.each(p_button, function (index, item) {
+        $(this).click(function () {
+            p_button.removeClass('active');
+            p_button.eq(index).addClass('active');
+            p_contents.hide();
+            p_contents.eq(index).show();
+        });
     });
 });
